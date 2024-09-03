351a7408e8fe7a6e8095c94482218e63f53ff760
AmaniBoukil <amani.boukil@campion-tech.com>
2024-09-03 16:36:23 +0100

    Update

---
+++ reports/__manifest__.py
@@ -0,0 +1,27 @@
+{
+    'name': '' ,
+    'version': '1.0',
+    'category': '',
+    'author': '',
+    'summary': '',
+    'description': """""",
+    'depends': [],
+    'data': [
+        'security/ir.model.access.csv',
+        'report/contact_card.xml',
+        'report/emp_card.xml',
+        'report/test_template_report.xml',
+
+    ],
+
+    'depends': ['base'], # installation de modules / independences
+    'demo': [],
+
+    'installable': True,
+    'auto_install': False,
+    'application': True,
+    'sequence': -100,
+    'license':'LGPL-3',
+
+     'assets': {},
+}
